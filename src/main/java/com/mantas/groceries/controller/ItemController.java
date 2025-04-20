c37446652ebb7fa4d6915440e02a700608b1f43f
mantasg6 <mantasgricius6@gmail.com>
2025-04-20 08:07:40 -0400

    implement CRUD controller, repository and service

--- src/main/java/com/mantas/groceries/controller/ItemController.java
+++ src/main/java/com/mantas/groceries/controller/ItemController.java
@@ -1,18 +1,46 @@
 package com.mantas.groceries.controller;
 
-import com.mantas.groceries.model.Category;
 import com.mantas.groceries.model.Item;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mantas.groceries.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/items")
 public class ItemController {
 
-    @GetMapping("/api/v1/items")
-    public Item items() {
-        return new Item(1, "Apple", Category.FRUIT, BigDecimal.valueOf(1.25), 10);
+    private final ItemService itemService;
+
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public List<Item> getAllItems() {
+        return itemService.getAllItems();
+    }
+
+    @GetMapping("/{id}")
+    public Item getItemByID(@PathVariable int id) {
+        return itemService.getItemByID(id);
+    }
+
+    @PostMapping
+    public Item createItem(@RequestBody Item item) {
+        return itemService.createItem(item);
+    }
+
+    @PutMapping
+    public Item updateItem(@RequestBody Item item) {
+        return itemService.updateItem(item);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id) {
+        itemService.deleteItem(id);
     }
 
 }
---
+++ src/main/java/com/mantas/groceries/service/ItemService.java
@@ -0,0 +1,39 @@
+package com.mantas.groceries.service;
+
+import com.mantas.groceries.model.Item;
+import com.mantas.groceries.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item getItemByID(int id) {
+        return itemRepository.findById(id).orElse(null);
+    }
+
+    public Item createItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public Item updateItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public void deleteItem(int id) {
+        itemRepository.deleteById(id);
+    }
+}
